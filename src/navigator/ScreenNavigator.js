b0bda064157c3147ae769ade983f89f34fec0654
NaumanRecat <naumantariq2977@gmail.com>
2021-09-23 00:29:28 -0700

    screen navigator

---
+++ src/navigator/ScreenNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { SignIn } from '../screen/SignIn';
+
+
+const Stack = createNativeStackNavigator();
+
+export const ScreenNavigator =()=>  {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screen/SignIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+export class SignIn extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Pakistan
+                </Text>
+            </View>
+        )
+    }
+}
