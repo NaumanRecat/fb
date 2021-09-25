9005e870f08af6b7bf5e09ced7815a03751a3928
NaumanRecat <naumantariq2977@gmail.com>
2021-09-25 13:27:12 -0700

    ath

--- src/screen/SignIn.js
+++ src/screen/SignIn.js
@@ -1,11 +1,34 @@
 import React from "react";
-import {View, Text, StyleSheet, Image, TextInput, Button, TouchableOpacity, alert} from 'react-native';
+import {View, Text, StyleSheet, Image, TextInput, Button, TouchableOpacity, Alert} from 'react-native';
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
 
 
-import { Signup } from "./Signup";
+import { Signup } from '../screen/Signup';
 
 export class SignIn extends React.Component{
+
+    state = {
+        name: '',
+        fname: '',
+        email: '',
+        phone: '',    
+      };
+
+      useradd = () => {
+        const {name, fname, email, phone} = this.state;
+    
+        if (name === '' || fname === '' || email === '' || phone === '') {
+          Alert.alert('Fill all fields');
+        } else {
+          const getData = {
+            userName: this.state.name.toUpperCase(),
+            fatherName: this.state.fname.toUpperCase(),
+            useremail: this.state.email.toLowerCase(),
+            userPhone: this.state.phone,
+          };
+          this.props.navigation.navigate('Signup', {values: getData});
+        }
+      };
 
     render(){
         return(
@@ -18,14 +41,14 @@ export class SignIn extends React.Component{
                 </View>
 
             <View style={styles.InputContainer}>
-                <TextInput style={styles.TextInput} placeholder={'Name'} />
-                <TextInput style={styles.TextInput2} placeholder={'Father Name'}/>
-                <TextInput style={styles.TextInput2} placeholder={'Email'}/>
-                <TextInput style={styles.TextInput2} placeholder={'password'}/>
+                <TextInput onChangeText={(name) => this.setState({name})} value={this.state.name} style={styles.TextInput} placeholder={'Name'} />
+                <TextInput onChangeText={(fname) => this.setState({fname})} value={this.state.fname} style={styles.TextInput2} placeholder={'Father Name'}/>
+                <TextInput onChangeText={(email) => this.setState({email})} value={this.state.email} style={styles.TextInput2} placeholder={'Email'}/>
+                <TextInput onChangeText={(phone) => this.setState({phone})} value={this.state.phone} style={styles.TextInput2} placeholder={'phone'}/>
             </View>
 
             <View style={styles.Login} >
-            <TouchableOpacity  onPress={() => { this.props.navigation.navigate('Signup') }} style={styles.ButtonLogin} >
+            <TouchableOpacity   onPress={() => { this.useradd(); }} style={styles.ButtonLogin} >
              <Text style={styles.LoginText}> Log In</Text>
             </TouchableOpacity>
             <Text style={styles.Forgotpassword} >Forgotten Password?</Text>
