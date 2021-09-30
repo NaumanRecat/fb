6c15c6d968bd224d5ac6fb17db2ecb0b886c8c7d
NaumanRecat <naumantariq2977@gmail.com>
2021-09-30 01:41:48 -0700

    nomi

--- src/screen/State.js
+++ src/screen/State.js
@@ -1,10 +1,26 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 export class State extends React.Component{
+    state={
+        name:'ali',
+        age: '25',
+    }
     render (){
+        // let name = 'Mano'
         return(
-            <Text> LearN State </Text>
+            <View>
+                {
+                    this.state.name ? <View><Text> {this.state.name} </Text></View>: <View><Text> Mahnoor ky Boobs </Text></View>
+                }
+                <Button
+                title="Change Name"
+                color="green"
+                onPress={()=>{
+                    this.setState({name:""})
+                }}
+                />
+            </View>
         )
     }
 }
