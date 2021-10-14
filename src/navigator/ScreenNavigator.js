66e09047f949644ffc91171a7616303e245a8138
NaumanRecat <naumantariq2977@gmail.com>
2021-10-14 09:19:22 -0700

    ok

--- src/navigator/ScreenNavigator.js
+++ src/navigator/ScreenNavigator.js
@@ -6,6 +6,7 @@ import { SignIn } from '../screen/SignIn';
 import {Signup} from '../screen/Signup';
 import { Dashboard } from '../screen/Dashboard';
 import {State} from '../screen/State';
+import Api from '../screen/Api'
 
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ export const ScreenNavigator =()=>  {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Api" component={Api}  options={{headerShown:false}} />
       <Stack.Screen name="State" component={State}  options={{headerShown:false}} />
       <Stack.Screen name="Dashboard" component={Dashboard}  options={{headerShown:false}} />
       <Stack.Screen name="SignIn" component={SignIn}  options={{headerShown:false}} />
